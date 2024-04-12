db11396fa2c8b3ed410261d0005138d70cf6c4f6
zyw <1727448271@qq.com>
2024-04-12 21:14:37 +0800

    ....

--- server/src/main/java/com/glimmer/server/RequirementService.java
+++ server/src/main/java/com/glimmer/server/RequirementService.java
@@ -4,11 +4,12 @@ package com.glimmer.server;
 
 
 import com.glimmer.Request.Requirement;
+import com.glimmer.dto.Recommendation;
 import com.glimmer.entity.RecommendUser;
 
 import java.util.List;
 
 public interface RequirementService {
 
-    RecommendUser sendRequirement(Requirement requirement);
+    List<Recommendation> sendRequirement(Requirement requirement);
 }
--- server/src/main/java/com/glimmer/server/impl/RequirementServiceImpl.java
+++ server/src/main/java/com/glimmer/server/impl/RequirementServiceImpl.java
@@ -4,7 +4,14 @@ package com.glimmer.server.impl;
 import cn.hutool.json.JSONUtil;
 import com.glimmer.Request.Requirement;
 import com.glimmer.clients.SendMessageClient;
+import com.glimmer.dto.BaseInfo;
+import com.glimmer.dto.EducationInfo;
+import com.glimmer.dto.Recommendation;
 import com.glimmer.entity.RecommendUser;
+import com.glimmer.entity.User;
+import com.glimmer.exception.BaseException;
+import com.glimmer.mapper.AskMapper;
+import com.glimmer.mapper.IdMapper;
 import com.glimmer.server.RequirementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,11 +24,31 @@ public class RequirementServiceImpl implements RequirementService {
     @Autowired
     private SendMessageClient sendMessageClient;
 
-    @Override
-    public RecommendUser sendRequirement(Requirement requirement) {
+    @Autowired
+    private IdMapper idMapper;
 
-        String message = sendMessageClient.sendRequirement(requirement);
-        RecommendUser recommendUser = JSONUtil.toBean(message,RecommendUser.class);
-        return recommendUser;
+    @Autowired
+    private AskMapper askMapper;
+
+    @Override
+    public List<Recommendation> sendRequirement(Requirement requirement) {
+
+        List<Recommendation> recommendationList = null;
+        RecommendUser recommendUser = sendMessageClient.sendRequirement(requirement);
+        for(String username:recommendUser.getUsername()){
+            User user = idMapper.selectUser(username);
+            BaseInfo baseInfo = askMapper.FindBaseHistory(user.getId());
+            EducationInfo educationInfo = askMapper.FindEducationHistory(user.getId());
+            Integer numWork = askMapper.FindWorkHistory(user.getId()).getExperience().length;
+            Integer numProject = askMapper.FindProjectHistory(user.getId()).getExperience().length;
+            Recommendation recommendation = new Recommendation(username,user.getPhone(),baseInfo.getIdentity(),educationInfo.getSchool(),educationInfo.getMajor(),numWork,numProject,educationInfo.getNature());
+            try {
+                recommendationList.add(recommendation);
+            } catch (Exception e ) {
+                throw new BaseException("推荐出错");
+            }
+        }
+
+        return recommendationList;
     }
 }
