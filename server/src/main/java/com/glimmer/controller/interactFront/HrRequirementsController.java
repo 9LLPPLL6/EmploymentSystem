01a3cc52829593fa459b066e2421a076dca390cc
zyw <1727448271@qq.com>
2024-04-12 20:28:58 +0800

    修改了hr模块

--- server/src/main/java/com/glimmer/controller/interactFront/HrRequirementsController.java
+++ server/src/main/java/com/glimmer/controller/interactFront/HrRequirementsController.java
@@ -1,7 +1,9 @@
 package com.glimmer.controller.interactFront;
 
 
+import cn.hutool.json.JSONUtil;
 import com.glimmer.Request.Requirement;
+import com.glimmer.dto.OfficerInfo;
 import com.glimmer.entity.RecommendUser;
 import com.glimmer.result.Result;
 import com.glimmer.server.RequirementService;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -36,5 +39,18 @@ public class HrRequirementsController {
                 .message(recommendation.toString())
                 .build();
         return Result.success("成功",statusVo);
+    }
+
+    @PostMapping("/req")
+    List<OfficerInfo> UpLoad(@RequestBody Requirement requirement){
+        List<OfficerInfo> officerInfos = new ArrayList<>();
+        officerInfos.add(new OfficerInfo("李俊霖","机器学习工程师","掌握python，熟悉pytorch，有CVPR paper，曾获得ACM金牌","30-60k*15薪","1234567890"));
+        officerInfos.add(new OfficerInfo("张三","机器学习工程师","熟悉pytorch，掌握python，有ACL paper，熟悉NLP","30-60k*15薪","1982349987"));
+        officerInfos.add(new OfficerInfo("李四","机器学习工程师","掌握C++，掌握python，熟悉pytorch，有IJCAI paper","20-60k*15薪","1789283445"));
+        officerInfos.add(new OfficerInfo("李和","机器学习工程师","掌握python，熟悉TensorFlow，熟悉pytorch，有AAAI paper","25-50k*15薪","1223898767"));
+        officerInfos.add(new OfficerInfo("王中","机器学习工程师","熟悉pytorch，熟悉Linux，掌握python，有SIGIR paper","20-45k*15薪","1987272777"));
+        officerInfos.add(new OfficerInfo("郑同","机器学习工程师","开源贡献者，github源码者，掌握python，熟悉golang，有EMNLP paper，熟悉pytorch，","25-45k*15薪","12029292922"));
+
+        return officerInfos;
     }
 }
